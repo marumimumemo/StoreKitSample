80fca05ac460114df99c32087c7a09c192678a0f
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-04-01 18:41:53 +0900

    StoreKitSample

--- StoreKitSample/ViewController.swift
+++ StoreKitSample/ViewController.swift
@@ -6,15 +6,29 @@
 //  Copyright © 2020 satoshi.marumoto. All rights reserved.
 //
 
+import StoreKit
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func requestReviewButton(_ sender: UIButton) {
+        
+        if #available(iOS 10.3, *) {
+            SKStoreReviewController.requestReview()
+        } else {
+            if let url = URL(string: "itms-apps://itunes.apple.com/app/{appleid}?action=write-review") {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:])
+                } else {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }
+    }
 
 }
 
